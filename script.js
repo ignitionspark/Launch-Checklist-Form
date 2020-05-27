24ed9cf18441a2075c9f4381ae2854234b80bc6b
unknown <lbonfonte@gmail.com>
2020-05-27 00:36:42 -0400

    God willing this works

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 // Write your JavaScript code here!
 window.addEventListener("load", function () {
     let launchAPI = "https://handlers.education.launchcode.org/static/planets.json";
@@ -21,10 +19,10 @@ fetch(launchAPI).then(function(response){
     form.addEventListener("submit", function (event) {
         event.preventDefault();
    
-let pilotInput = document.querySelector("input[name=pilotName]").value;
-let copilotInput = document.querySelector("input[name=copilotName]").value;
-let fuelInput = document.querySelector("input[name=fuelLevel]").value;
-let massInput = document.querySelector("input[name=cargoMass]").value;
+let pilotInput = document.querySelector("input[name=pilotName]");
+let copilotInput = document.querySelector("input[name=copilotName]");
+let fuelInput = document.querySelector("input[name=fuelLevel]");
+let massInput = document.querySelector("input[name=cargoMass]");
 let pilotStatus = document.getElementById("pilotStatus");
 let copilotStatus = document.getElementById("copilotStatus");
 let fuelStatus = document.getElementById("fuelLevel");
@@ -40,29 +38,29 @@ if (pilotInput.value === "" || copilotInput.value === "" || fuelInput.value === 
                 alert("You data does meet requirements. Please enter valid response");
      }
        
-        if (isNaN(pilot.value) && isNaN(copilot.value) && !isNaN(fuel.value) && !isNaN(cargo.value)) {
+        if (isNaN(pilotInput.value) && isNaN(copilotInput.value) && !isNaN(fuelInput.value) && !isNaN(massInput.value)) {
+           
             pilotStatus.innerHTML = `Pilot: ${pilotInput.value} is ready for launch!`;
             copilotStatus.innerHTML = `Co-pilot ${copilotInput.value} is ready for launch!`;
        
             if (fuelInput.value < 10000) {
+                
                 faultyItems.style.visibility = "visible";
                 launchStatus.style.color = "red";
                 launchStatus.innerHTML = "Shuttle not ready for launch!";
                 fuelStatus.innerHTML = "Fuel is too low for takeoff";
-                } else {
-                fuelStatus.innerHTML = "Fuel level high enough for launch!";
-                }
+            };
        
                 if (massInput.value > 10000) {
-                   faultyItems.style.visibility = "visible";
+                   
+                    faultyItems.style.visibility = "visible";
                    launchStatus.style.color = "red";
                    launchStatus.innerHTML = "Shuttle not ready for launch!";
-                   cargoStatus.innerHTML = "Too much mass to lift off. Please use the jettison method.";
-                } else {
-                   cargoStatus.innerHTML = "Cargo mass low enough for launch!";
+                   cargoStatus.innerHTML = "Too much mass to lift off. Please remove excess weight.";
                 }
        
                     if (fuelInput.value >= 10000 && massInput.value <= 10000) {
+                        
                     faultyItems.style.visibility = "visible";
                     launchStatus.style.color = "green";
                     launchStatus.innerHTML = "Shuttle is ready for launch!!";
@@ -74,4 +72,5 @@ if (pilotInput.value === "" || copilotInput.value === "" || fuelInput.value === 
         });
     });
 
-    
+   
+  
